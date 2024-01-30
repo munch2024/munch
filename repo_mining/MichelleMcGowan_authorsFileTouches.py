287cd3513af7ed8eb037d143c66eb9eac684f5ae
Michael Razon <ocmichaelrazon@gmail.com>
2024-01-30 00:10:30 -0800

    Adding my Git and GitHub Lab
    Adding my CollectFiles, authorFileTouches, and scatterplot scripts.
    Also contains executive summary.

---
+++ repo_mining/MichelleMcGowan_authorsFileTouches.py
@@ -0,0 +1,111 @@
+import json
+import requests
+import csv
+import pathlib
+import os
+
+if not os.path.exists("data"):
+ os.makedirs("data")
+
+# GitHub Authentication function
+def github_auth(url, lsttoken, ct):
+    jsonData = None
+    try:
+        ct = ct % len(lstTokens)
+        headers = {'Authorization': 'Bearer {}'.format(lsttoken[ct])}
+        request = requests.get(url, headers=headers)
+        jsonData = json.loads(request.content)
+        ct += 1
+    except Exception as e:
+        pass
+        print(e)
+    return jsonData, ct
+
+# @dictFiles, empty dictionary of files
+# @lstTokens, GitHub authentication tokens
+# @repo, GitHub repo
+def countfiles(dictfiles, lsttokens, repo, dates, authors):
+    ipage = 1  # url page counter
+    ct = 0  # token counter
+    repoLangs = [".java", ".cpp", ".kt", ".h"]
+
+    try:
+        # loop though all the commit pages until the last returned empty page
+        while True:
+            spage = str(ipage)
+            commitsUrl = 'https://api.github.com/repos/' + repo + '/commits?page=' + spage + '&per_page=100'
+            jsonCommits, ct = github_auth(commitsUrl, lsttokens, ct)
+
+            # break out of the while loop if there are no more commits in the pages
+            if len(jsonCommits) == 0:
+                break
+            # iterate through the list of commits in  spage
+            for shaObject in jsonCommits:
+                sha = shaObject['sha']
+                # For each commit, use the GitHub commit API to extract the files touched by the commit
+                shaUrl = 'https://api.github.com/repos/' + repo + '/commits/' + sha
+                shaDetails, ct = github_auth(shaUrl, lsttokens, ct)
+                filesjson = shaDetails['files']
+                fileAuthor = shaDetails['commit']['author']['name']                       
+                fileDate = shaDetails['commit']['author']['date'][0:10]                                      
+                for filenameObj in filesjson:
+                    filename = filenameObj['filename']
+                    # get file extensions 
+                    fileExt = pathlib.Path(filename).suffix
+                    # check if in the repoLangs 
+                    if(fileExt in repoLangs):   
+                        dictfiles[filename] = dictfiles.get(filename, 0) + 1                                                                                       
+                        authors.setdefault(filename, [])
+                        authors[filename].append([fileAuthor, fileDate])                                                                  
+                        #print(filename)                                        
+                        #print(fileAuthor)                                        
+            ipage += 1
+    except:
+        print("Error receiving data")
+        exit(0)
+# GitHub repo
+repo = 'scottyab/rootbeer'
+# repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
+# repo = 'k9mail/k-9' # This repo is commit heavy. It takes long to finish executing
+# repo = 'mendhak/gpslogger'
+
+
+# put your tokens here
+# Remember to empty the list when going to commit to GitHub.
+# Otherwise they will all be reverted and you will have to re-create them
+# I would advise to create more than one token for repos with heavy commits
+lstTokens = [""]
+
+dictfiles = dict()
+authors = dict()
+dates = dict()
+countfiles(dictfiles, lstTokens, repo, dates, authors)
+print('Total number of files: ' + str(len(dictfiles)))
+
+file = repo.split('/')[1]
+# change this to the path of your file
+fileOutput = 'data/file_' + file + '.csv'
+rows = ["Filename", "Touches"]
+fileCSV = open(fileOutput, 'w')
+writer = csv.writer(fileCSV)
+writer.writerow(rows)
+
+touches = 0
+for filename, count in dictfiles.items():
+    rows = [filename, count]
+    writer.writerow(rows)
+    touches += count
+fileCSV.close()
+
+fileOutput2 = 'data/file_authors' + '.csv'
+rows2 = ["Filename", "Author", "Date"]
+fileCSV2 = open(fileOutput2, 'w')
+writer2 = csv.writer(fileCSV2)
+writer2.writerow(rows2)
+
+for filename, vals in authors.items():
+    for value in vals:
+        rows2 = [filename, value[0], value[1]]
+        writer2.writerow(rows2)
+fileCSV2.close()
+print('The files' + ' has been touched ' + str(touches) + ' times.')
