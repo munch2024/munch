befa912d7d84c0c82fe48124785c0741dd1e0c9c
Michael Razon <ocmichaelrazon@gmail.com>
2024-01-30 00:03:37 -0800

    Adding my Git and GitHub Lab
    Adding my CollectFiles, authorFileTouches, and scatterplot scripts. Also contains executive summary.

--- repo_mining/Edward_bargraph.py
+++ repo_mining/Edward_bargraph.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,25 +18,3 @@ plt.savefig('data/bargraph.png', dpi=300)
 
 # Show the bar graph
 plt.show()
-=======
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Assuming your CSV data is in a file named 'data.csv'
-df = pd.read_csv('data/author_rootbeer.csv')
-
-# Count the occurrences of each author
-author_counts = df['Author'].value_counts()
-
-# Plotting the bar graph
-author_counts.plot(kind='bar', color='blue')
-plt.title('Author Frequency')
-plt.xlabel('Author')
-plt.ylabel('Frequency')
-plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better visibility
-plt.tight_layout()
-plt.savefig('data/bargraph.png', dpi=300)
-
-# Show the bar graph
-plt.show()
->>>>>>> 4aa338990eef2bbdd5512281c65a880c9adbcf50
