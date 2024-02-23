69fc33f0d63db151044ae76fc879f5a377ae3dc6
haiimkeith <haiimkeith@gmail.com>
2024-02-23 00:21:12 -0800

    Fixes #56

--- ChatGPT/task_2.3/KeithDelRosario_PythagTheorem.py
+++ ChatGPT/task_2.3/KeithDelRosario_PythagTheorem.py
@@ -4,7 +4,28 @@
 import math
 
 def pythagorean_theorem(a, b):
-    return math.sqrt(a**2 + b**2)
+    """
+    Calculate the length of the hypotenuse of a right-angled triangle using the Pythagorean theorem.
+
+    Args:
+        a (float): Length of one side of the triangle.
+        b (float): Length of the other side of the triangle.
+
+    Returns:
+        float: Length of the hypotenuse.
+    """
+
+    # Alternative solution that provides further readability for the user
+    # return math.sqrt(a**2 + b**2)
+    # Into
+    # return math.sqrt(a * a + b * b)
+
+    # Using math library, we can optimize the code further and provide an alternate solution
+    # that would increase readbility and efficiency of the code snippet while also being more
+    # beginner friendly, giving the two parameters and having the math.hypot() function calculate
+    # the hypotenuse if the user doesn't understand the Pythagorean theorem.
+    return math.hypot(a, b)
+    
 
 # Example usage:
 side1 = 3
