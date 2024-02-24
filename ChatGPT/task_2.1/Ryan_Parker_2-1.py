e167b0f1ae7dd74ae1311bd85a1bb909c1de9b3f
Ryan Parker <rparker52003@gmail.com>
2024-02-24 15:10:19 -0800

    Updated task 2.1 code with chatGPT refactored code

--- ChatGPT/task_2.1/Ryan_Parker_2-1.py
+++ ChatGPT/task_2.1/Ryan_Parker_2-1.py
@@ -4,12 +4,7 @@
 def is_prime(n):
     if n <= 1:
         return False
-    elif n == 2:
-        return True
-    elif n % 2 == 0:
-        return False
-    else:
-        for i in range(3, int(n**0.5) + 1, 2):
-            if n % i == 0:
-                return False
-        return True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
