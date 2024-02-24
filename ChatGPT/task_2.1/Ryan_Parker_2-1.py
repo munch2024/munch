273799c9c5c81fd984ddb5a0768748b2fddb1da0
Ryan Parker <rparker52003@gmail.com>
2024-02-24 14:15:56 -0800

    Added task 2.1 code snippet

---
+++ ChatGPT/task_2.1/Ryan_Parker_2-1.py
@@ -0,0 +1,15 @@
+# Ryan Parker
+# Task 2.1 Code Snippet
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(n**0.5) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
