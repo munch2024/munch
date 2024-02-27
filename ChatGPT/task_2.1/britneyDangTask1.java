2624d09d04fb4456365a1deea6b5d6fae1ab2f22
honeepuffs <136287986+honeepuffs@users.noreply.github.com>
2024-02-26 20:23:09 -0800

    Update britneyDangTask1.java

--- ChatGPT/task_2.1/britneyDangTask1.java
+++ ChatGPT/task_2.1/britneyDangTask1.java
@@ -1,19 +1,20 @@
-private Optional<Player> playerWithMostCards()
-{
+private Optional<Player> playerWithMost(Predicate<Card> condition) {
     Player scoringPlayer = null;
     int maxCount = 0;
 
-    for(Player p : players)
-    {
-        final int count = p.pile.size();
+    for (Player p : players) {
+        int count = 0;
 
-        if(count > maxCount)
-        {
+        for (Card c : p.pile) {
+            if (condition.test(c)) {
+                count++;
+            }
+        }
+
+        if (count > maxCount) {
             scoringPlayer = p;
             maxCount = count;
-        }
-        else if(count == maxCount)
-        {
+        } else if (count == maxCount) {
             scoringPlayer = null;
         }
     }
@@ -21,64 +22,14 @@ private Optional<Player> playerWithMostCards()
     return Optional.ofNullable(scoringPlayer);
 }
 
-private Optional<Player> playerWithMostSevens()
-{
-    Player scoringPlayer = null;
-    int maxCount = 0;
 
-    for(Player p : players)
-    {
-        int count = 0;
-
-        for(Card c : p.pile)
-        {
-            if(c.is(Card.Value.SEVEN))
-            {
-                count++;
-            }
-        }
-
-        if(count > maxCount)
-        {
-            scoringPlayer = p;
-            maxCount = count;
-        }
-        else if(count == maxCount)
-        {
-            scoringPlayer = null;
-        }
+private void updateScores() {
+    for (Player p : players) {
+        p.score = p.scopas;
     }
 
-    return Optional.ofNullable(scoringPlayer);
-}
+    playerWithMost(p -> true).ifPresent(p -> p.score += 1); // most cards
+    playerWithMost(c -> c.is(Card.Value.SEVEN)).ifPresent(p -> p.score += 1); // most sevens
+    playerWithMost(c -> c.is(Card.Suit.SPADES)).ifPresent(p -> p.score += 1); // most spades
 
-private Optional<Player> playerWithMostSpades()
-{
-    Player scoringPlayer = null;
-    int maxCount = 0;
-
-    for(Player p : players)
-    {
-        int count = 0;
-
-        for(Card c : p.pile)
-        {
-            if(c.is(Card.Suit.SPADES))
-            {
-                count++;
-            }
-        }
-
-        if(count > maxCount)
-        {
-            scoringPlayer = p;
-            maxCount = count;
-        }
-        else if(count == maxCount)
-        {
-            scoringPlayer = null;
-        }
-    }
-
-    return Optional.ofNullable(scoringPlayer);
 }
